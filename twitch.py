5e7c88396b6c064526d9363f27ca679840568f6b
AllanVester <vesterallan246@gmail.com>
2025-02-09 12:07:37 +0100

    Updated twitch.py

--- twitch.py
+++ twitch.py
@@ -1566,7 +1566,7 @@ class Twitch:
                         if (
                             "message" in error_dict
                             and error_dict["message"] in (
-                                # "service error",
+                                "service error",
                                 "service unavailable",
                                 "service timeout",
                                 "context deadline exceeded",
